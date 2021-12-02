81882bc8a0a61dfe265a8a1eb8ccb2c74796df26
unknown <nirupama1210@gmail.com>
2021-12-02 22:49:56 +0530

    PROGRAD 5622

---
+++ first_app/src/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App1 from './App';
+function App()
+{
+  return <div>Hey Prograds! Welcome to React Learning</div>
+}
+ReactDOM.render(
+<div><App/><App1/></div>,
+  document.getElementById('root')
+);
+
+
